f49615d430ab7f871ac88d0f58f7189eab323ea2
Ruben Verster <ruben.verster@younglings.africa>
2020-01-28 13:59:08 +0200

    final gatsby site, blogTemplate adjusted

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,7 +24,7 @@ const path = require("path")
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const blogTemplate = path.resolve("./src/templates/blogTemplate.js")
 
   const response = await graphql(`
     query {
